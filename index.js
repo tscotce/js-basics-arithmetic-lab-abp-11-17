3fcd6e48b9110a50b7ad0f869a1e46f5aaeaf7e9
tscotce <tscotce@gmail.com>
2017-11-16 04:59:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.floor(Math.random() * 20);
 let randomInteger = Math.round(randomNumber);
-let randomUserID = Math.floor(Math.random()) * 1000000020) + 1000000001;
+//* let randomUserID = Math.floor(Math.random()) * 1000000020) + 1000000001;
